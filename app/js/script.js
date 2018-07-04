646754080f5025a8186c6638e3fa8dde218b9f25
Andrew Kim <jeesoo2002@gmail.com>
2018-07-03 18:34:39 -0700

    init

---
+++ app/js/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var person = {
+  name: "Ryan Christiani"
+};
+
+var testString = "My name is " + person.name;
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  useref = require("gulp-useref"),
+  cssnano = require("gulp-cssnano"),
+  gulpif = require("gulp-if"),
+  babel = require("gulp-babel"),
+  uglify = require("gulp-uglify");
+
+gulp.task("sass", function() {
+  return gulp
+    .src("app/scss/**/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("app/css"));
+});
+
+gulp.task("useref", function() {
+  return gulp
+    .src("app/*.html")
+    .pipe(useref())
+    .pipe(gulpif("*.js", uglify()))
+    .pipe(gulpif("*.css", cssnano()))
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("scripts", function() {
+  return gulp
+    .src("app/js/**/*.js")
+    .pipe(
+      babel({
+        presets: ["es2015"]
+      })
+    )
+    .pipe(gulp.dest("dist"));
+});
+
+// gulp.task("clean", function(cb) {
+//   del(["dist"], cb);
+// });
+
+gulp.task("watch:files", function() {
+  gulp.watch("app/scss/**/*.scss", ["sass"]);
+  gulp.watch("app/js/**/*.js", ["scripts"]);
+  gulp.watch("app/*.html", ["useref"]);
+  //   gulp.watch("app/*.html", ["clean"]);
+});
+
+gulp.task("default", ["watch:files", "useref"]);
